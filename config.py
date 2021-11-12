9d5b75aa12e828ebd7e639942a3cebf479c1a567
Nash Boisvert <fenderfan@live.ca>
2021-11-11 19:54:34 -0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-﻿config = {
-    "bot_prefix": "!",
-    "token": "OTA4NDMzNDQxMTAyNjMwOTMz.YY1qkg.7qjqBFoGYP0YmxuZJQX4HkDnVtE",
-    "application_id": "908433441102630933",
-    "owner": "305961086782275586"
-}
