5668bb3d528260a3491ede9e602412a5fdc96513
Nash Boisvert <fenderfan@live.ca>
2021-11-11 18:27:12 -0700

    restructed methods in on_message

--- bot.py
+++ bot.py
@@ -1,13 +1,6 @@
-﻿import json
-import os
-import platform
-import random
-import sys
-import bot_methods
+﻿import bot_methods
 import logging
 import discord
-from discord.ext import tasks
-from discord.ext.commands import Bot
 
 from config import config
 
@@ -21,16 +14,17 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if not message.author == client.user:
-            if message.content == '!help':
-                print('Help method requested, firing')
-                help_message = bot_methods.format_helper()
-                await message.channel.send(help_message)
             prob = bot_methods.code_detection(message)
             print(message.content)
             if prob >= 3:
                 await message.channel.send('Hey! Hope you don\'t mind me, I\'m just formatting your code for you! To '
-                                           'learn more about this, type !help\n...' + '```' + message.content + '```')
+                                           'learn more about this, type !help\n...' + '```' + str(
+                    message.content) + '```')
             print(f'Code detection probability: {prob}')
+        elif message.content == '!!help':
+            print('Help method requested, firing')
+            help_message = bot_methods.format_helper()
+            await message.channel.send(help_message)
 
 
 client = MyClient()
--- bot_methods.py
+++ bot_methods.py
@@ -5,30 +5,27 @@ def code_detection(message):
     code_symbols = ['<', '<=', '>', '>=', '=', '==', '||', '&&', '[', ']', '{', '}', '(', ')', ';', ':', '->']
     data_types = ['string', 'int', 'double', 'bool', 'float', 'long', 'char']
     prob = 0
-    detection_index = []
 
     message_split = message.content.split()
-    if message_split.contains('```'):
+    if '```' in message_split:
         return 'Message already formatted'
     else:
-        for i in range(len(message_split)-1):
+        for i in range(len(message_split) - 1):
             if message_split[i] in data_types:
                 prob += 1
-                detection_index.append(i)
             for character in message_split[i]:
                 if character in code_symbols:
                     prob += 1
-                    detection_index.append(i)
 
-    return prob, detection_index[0]
+    return prob
 
 
 def format_helper():
     syntax_highlighters = {'JavaScript': 'js', 'CSS': 'css', 'C#': 'cs', 'HTML': 'html', 'Python': 'py', 'JSON': 'json',
                            'SQL': 'sql'}
 
-    message_content = 'Here is how you can format for yourself!``````<languagecode>\n//Your code goes ' \
-                      'here\n//Remember to not seperate with a space!\n//Available codes are...\n\n'
+    message_content = 'Here is how you can format for yourself!``````<language_code>\n//Your code goes ' \
+                      'here\n//Remember to not separate with a space!\n//Available codes are...\n\n'
     for i in syntax_highlighters:
         message_content += f'{i} code = {syntax_highlighters[i]}\n'
 
