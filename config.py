bd64cf41d83e8c05e2aed4389e034ca099e0171f
Nash Boisvert <fenderfan@live.ca>
2021-11-11 19:39:28 -0700

    fixed detection

--- config.py
+++
@@ -1,6 +0,0 @@
-﻿config = {
-    "bot_prefix": "!",
-    "token": "OTA4NDMzNDQxMTAyNjMwOTMz.YY1qkg.H7s4F1TetB9tck47fn24KuxJpjY",
-    "application_id": "908433441102630933",
-    "owner": "305961086782275586"
-}
